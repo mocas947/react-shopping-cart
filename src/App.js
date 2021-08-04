6eff2a984944d00040c26d458a2de0e8b1320445
momcilo <momchileto@gmail.com>
2021-08-04 12:31:34 +0200

    482021-1231

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// samo da ovo smestim u feature-1 branč
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart/Katalog</a>
       </header>
+      <main>Product List/Raspoloživo</main>
+      <footer>Sva prava zadržana.</footer>
+
     </div>
   );
 }
